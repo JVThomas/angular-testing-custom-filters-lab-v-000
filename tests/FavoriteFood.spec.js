0c245411da1cbd282433c2c2d932b7956eca5adf
Justin Thomas <jthomas0209@gmail.com>
2016-04-04 09:40:57 -0400

    Done

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,25 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should filter out foods based on Favoritefood', function(){
+    var mockedList = [
+      {
+        name:"Test1",
+        favoriteFood: "pizza"
+      },
+      {
+        name: "Test2",
+        favoriteFood: "ice cream"
+      },
+      {
+        name:"Test3",
+        favoriteFood: "cupcakes"
+      }
+    ];
+    var results = $filter('favoriteFood')(mockedList, "pizza");
+    expect(results.length).toEqual(1);
+    expect(results[0].name).toEqual("Test1");
+  });
+
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,10 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should remove all vowels in a given string', function(){
+    var mockedString = "this is a test";
+    var results = $filter('removeAllVowels')(mockedString);
+    expect(results).toEqual("ths s  tst");
+  });
+
 });
